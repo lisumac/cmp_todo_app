80636c85aa79154168d2fccf03b9211a81bd8956
lisumac <lisadasmohapatra12@gmail.com>
2026-05-05 16:55:45 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/rdk/todo/App.kt
@@ -0,0 +1,13 @@
+package org.rdk.todo
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/rdk/todo/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.rdk.todo
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
