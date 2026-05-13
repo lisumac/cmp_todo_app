dca0e86a496988e37bf46c27c216a3d4c3381686
lisumac <lisadasmohapatra12@gmail.com>
2026-05-13 12:43:31 +0530

    fix the issue

--- composeApp/src/commonMain/kotlin/org/rdk/todo/data/ToDoInterface.kt
+++ composeApp/src/commonMain/kotlin/org/rdk/todo/data/ToDoInterface.kt
@@ -1,4 +1,15 @@
 package org.rdk.todo.data
 
+import kotlinx.coroutines.flow.Flow
+import org.rdk.todo.domain.ToDoTask
+import org.rdk.todo.utill.RequestState
+import kotlin.coroutines.CoroutineContext
+
 interface ToDoInterface {
+
+    fun createTask(task: ToDoTask): RequestState<Unit>
+    fun updateTask(task: ToDoTask): RequestState<Unit>
+    fun readSelectedTask(taskId: String): RequestState<ToDoTask>
+    fun readAllTask(task: CoroutineContext): Flow<RequestState<List<ToDoTask>>>
+    fun removeTask(taskId: String): RequestState<Unit>
 }
